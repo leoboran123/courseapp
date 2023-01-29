cfd2426f3ef286abb0cae436e54d658ce5bcf50d
leoboran123 <erdem_boran_badd@hotmail.com>
2023-01-29 18:58:20 +0300

    for döngüsü

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -8,20 +8,16 @@ data = {
     "web":"Web programlama kategorisindeki kurslar"
 }
 
-def index(req):
-    return render(req,"courses/index.html")
 
-def kurslar(req):
+
+def index(req):
     list_items=""
     category_list = list(data.keys())
 
-    for category in category_list:
-        redirect_url= reverse('courses_by_category', args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
+    return render(req,"courses/index.html", {
+        'categories':category_list
+    })
 
-    html = f"<h1>Kurs listesi</h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(html)
     
 def details(req, kurs_adi):
     return HttpResponse(kurs_adi)
