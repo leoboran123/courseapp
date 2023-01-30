d352733b8940ca693eba0a2befdb6a9b3fc966c3
leoboran123 <erdem_boran_badd@hotmail.com>
2023-01-30 19:27:17 +0300

    if else, filters

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.urls import reverse
@@ -8,14 +9,53 @@ data = {
     "web":"Web programlama kategorisindeki kurslar"
 }
 
+db = {
+    "courses":[
+        {
+        "title":"Javascript Kursu",
+        "description": "Javascript kurs açıklaması",
+        "imageUrl":"https://i.ytimg.com/vi/jS4aFq5-91M/maxresdefault.jpg",
+        "slug":"javascript",
+        "date":date(2022,10,10),
+        "isActive":True,
+        "isUpdated":True
+        },
+        {
+        "title":"Python Kursu",
+        "description": "Python kurs açıklaması",
+        "imageUrl":"https://www.upf.edu/documents/222910037/261564223/python2/7b73d5da-bcc5-72e0-db72-f1e763612931?t=1665491363773",
+        "slug":"python",
+        "date":date(2022,9,10),
+        "isActive":True,
+        "isUpdated":False
+        },
+        {
+        "title":"web geliştirme Kursu",
+        "description": "web geliştirme kurs açıklaması",
+        "imageUrl":"https://www.kodsihirbazi.com/wp-content/uploads/2018/03/Web-Geli%C5%9Ftirme-501x330.png",
+        "slug":"web-geliştirme",
+        "date":date(2022,8,10),
+        "isActive":True,
+        "isUpdated":True
+        }
+    ],
+    "categories": [
+            {"id":1, "name":"Programlama","slug":"programlama"},
+            {"id":2, "name":"Mobil Uygulama","slug":"mobil"},
+            {"id":3, "name":"Web Geliştirme","slug":"web"},
+        
+        ]
+}
+
 
 
 def index(req):
-    list_items=""
-    category_list = list(data.keys())
+    kurslar = db["courses"]
+    kategoriler = db["categories"]
 
     return render(req,"courses/index.html", {
-        'categories':category_list
+        'categories':kategoriler,
+        'courses':kurslar
     })
 
     
