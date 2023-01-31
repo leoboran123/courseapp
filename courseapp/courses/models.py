dad734e9c6a715149c1860eecd255e23ed9160e7
leoboran123 <erdem_boran_badd@hotmail.com>
2023-01-31 15:46:57 +0300

    model tanımlama

--- courseapp/courses/models.py
+++ courseapp/courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    imageUrl = models.CharField(max_length=50)
+    date = models.DateField()
+    isActive = models.BooleanField()
+
+
+
