4efa77ca0ca4f67babd861b84c826a5fa14d46e6
leoboran123 <erdem_boran_badd@hotmail.com>
2023-03-11 19:31:19 +0300

    enviroment variables

--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9-p@m3v7$+r=@je6)i@&wwqh#vz7fr+51r03m#)y@-5+8tps#'
+# SECRET_KEY = 'django-insecure-r9-p@m3v7$+r=@je6)i@&wwqh#vz7fr+51r03m#)y@-5+8tps#'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
